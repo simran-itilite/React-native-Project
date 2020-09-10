bf966ea30284b897536145fc7f662727b5390d14
simran-itilite <70706376+simran-itilite@users.noreply.github.com>
2020-09-10 16:49:10 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+//import file to add info
+
+import infoApp from './infoApp';
+
+export default function App() {
+  return (
+  <View>
+  </infoApp>
+  </View>
+  );
+  }
