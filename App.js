db53272ce146e17920a199b8f3aa1020c5f34fb5
simran-itilite <70706376+simran-itilite@users.noreply.github.com>
2020-09-10 17:25:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import { Text, View, StyleSheet } from 'react-native';
 
 //import file to add info
 
-import infoApp from './infoApp';
+import InfoApp from './infoApp';
 
 export default function App() {
   return (
   <View>
-  <infoApp/>
+  <InfoApp/>
   </View>
   );
   }
