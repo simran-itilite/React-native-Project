a58f4edea69984102aac94aebe76404623724186
simran-itilite <70706376+simran-itilite@users.noreply.github.com>
2020-09-10 16:26:37 +0530

    Update info.js

--- info.js
+++ info.js
@@ -1,42 +1,45 @@
 import * as React from 'react';
-import { Text,TextInput, View, StyleSheet } from 'react-native';
-
+import { Text,TextInput, View, StyleSheet } from 'react-native;
 
 export default function App() {
   return (
     <View>
-      <Text  style={{
-      marginTop:10 ,
-      textAlign: 'center',
-          height: 40,
-          borderColor: 'red',
-        
-  }}>
+      <Text  style={styles.header}>
         Lets Register YOU !!
       </Text>
-       <TextInput  style={{
-          height: 40,
-          margin: 5,
-          padding:10,
-          borderColor: 'red',
-          borderBottomWidth: 1
-        }}
-        defaultValue="Your Name">
+       <TextInput  style={styles.container}
+                 defaultValue="Your Name">
        
         </TextInput>
-         <TextInput style={{
-          height: 40,
-          margin: 5,
-          padding:10,
-          borderColor: 'red',
-          borderBottomWidth: 1
-        }}
-        defaultValue="Your Age" >
+         <TextInput style={styles.container}
+          defaultValue="Your Age" >
        
         </TextInput>
        </View>
   );
-}
+  }
+  const styles = StyleSheet.create({
+  container: 
+  {
+  height: 40,
+          margin: 5,
+          padding:10,
+          borderColor: 'red',
+          borderBottomWidth: 1
+           },
+ header:
+ {
+ color:'red',
+ margin : 15,
+ flex :1,
+ textAlign:'center',
+ fontFamily:'verdana',
+ fontSize:25,
+ padding:10
+ 
+ }
+ });    
+         
 
 
 
