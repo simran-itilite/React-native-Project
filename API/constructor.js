9b5713179d4108a312be843108099f17b38957d2
simran-itilite <70706376+simran-itilite@users.noreply.github.com>
2020-09-11 13:31:28 +0530

    Create constructor.js

---
+++ API/constructor.js
@@ -0,0 +1,14 @@
+//create constructor 
+constructor(props)
+{
+super(props);
+this.state =
+{
+//two states 
+//first state
+loading:true;
+//second state
+// create array(details) to store the values of JSON
+details:[]
+};
+}
