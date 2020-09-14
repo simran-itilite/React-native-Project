20129de097c1eb34037e0b0aadfde98d355190eb
simran-itilite <70706376+simran-itilite@users.noreply.github.com>
2020-09-14 23:18:44 +0530

    Create action.js

---
+++ SampleProject/action.js
@@ -0,0 +1,13 @@
+//EXPORT THE ACTIONS Types
+
+//display the content
+export const QUOTES_AVAILABLE = 'DATA_DISPLAY';
+
+//add content
+export const ADD_QUOTE = 'DATA_INSERT';
+
+//update content
+export const UPDATE_QUOTE = 'DATA_UPDATE';
+
+//deletion of content
+export const DELETE_QUOTE = 'DATA_DELETE';
