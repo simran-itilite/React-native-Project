5dc91c323ef55dcd2ab1672a321a11a78c8f029b
simran-itilite <70706376+simran-itilite@users.noreply.github.com>
2020-09-15 14:50:33 +0530

    Create action.js

---
+++ REDUX/action.js
@@ -0,0 +1,16 @@
+export const ADD_INFO = 'ADD_INFO';
+export const DELETE_INFO = 'DELETE_INFO';
+
+// Add info
+export const addinfo = (info) => ({
+    type: ADD_INFO,
+    data: {info}
+});
+
+
+//delete info
+export const delinfo = (id) => ({
+    type: DELETE_INFO,
+    data: {id}
+});
+
