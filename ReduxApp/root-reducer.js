eafcb995ba0ae9ce465f041b81bb7dcbd7ee82f3
simran-itilite <70706376+simran-itilite@users.noreply.github.com>
2020-09-16 12:10:57 +0530

    Update root-reducer.js

--- ReduxApp/root-reducer.js
+++ ReduxApp/root-reducer.js
@@ -1,1 +1,17 @@
+// component combinereducer used to combine reducers 
 
+//import from redux
+import {combineReducers} from 'redux';
+
+// from the reducers.js file import the reducer function
+
+import {reducer as userReducer} from './user/reducers';
+
+// combining reducers
+
+const reducer = combineReducers({
+  user: userReducer,
+});
+
+//export after combining the reducers
+export {reducer};
