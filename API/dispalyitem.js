97b010e05ad391d3f2272d48455f89f2c1044604
simran-itilite <70706376+simran-itilite@users.noreply.github.com>
2020-09-16 11:03:06 +0530

    Delete dispalyitem.js

--- API/dispalyitem.js
+++
@@ -1,9 +0,0 @@
-//render the item to dispaly
-renderItem=(details)=>
-//touchableopacity to respond to touches
-<TouchableOpacity>
-//display the items from json file like name, email,with their attributes name in json file
-  <Text>{details.item.name}</Text> 
-  <Text>{details.item.email}</Text>
-  <Text>{details.item.body}</Text>
-  </TouchableOpacity>
